e89720bdebb4b913617d1b7a3a536190129f1c05
ja4235 <hosodayugo@icloud.com>
2024-02-14 02:22:32 +0900

    関数にできそうなものを関数にしました．引数，戻り値どについては検討の余地あり

--- backend/index.js
+++ backend/index.js
@@ -43,15 +43,14 @@ const hospitalData = {
 };
 /*データベース*/
 
-/*関数*/
-
+/*ここから関数*/
 /*
 関数概要：緯度経度を用いて救急車から目的地の病院までの距離を算出し病院のリストを距離を注目し昇順にソートする関数
-引数：救急車が選択した科に当てはまる病院のドキュメントplaces
+引数1：救急車が選択した診療科に当てはまる病院のドキュメントplaces
+引数2：現在地origin
 戻り値: ソートされたリスト hospitalList 
 */
-function getSortHospitalList(places) {
-	const origin = new google.maps.LatLng(35.5988, 139.6506); //仮に救急車の現在地を都市大の位置とする
+async function getSortHospitalList(places,origin) {
 	const destinations = [];
 	let hospitalList = []; // 二重配列 [[病院ドキュメント, その病院と都市大との距離]]
 	//救急車の現在地から目的地（病院）の距離を算出し，destinationsにプッシュ
@@ -73,7 +72,7 @@ function getSortHospitalList(places) {
 					hospitalList.push([places[i], distance]);//病院のdocと距離をリストにプッシュ
 				}
 				hospitalList.sort((a, b) => a[1] - b[1]);//hospitalListを距離に注目して昇順ソート
-				console.log(hospitalList);
+				//console.log(hospitalList);
 				return hospitalList;
 			} else {
 				console.error('Error:', status);
@@ -82,6 +81,48 @@ function getSortHospitalList(places) {
 	});
 }
 
+/*
+関数概要：引数の診療科すべて当てはまる病院のリストを取得
+引数：診療科のリスト
+戻り値：引数のリストすべて満たす病院のリスト
+*/
+async function getHospitalsAnd(depArray){
+	const hospitalRef = collection(db, "hospital");
+	let queryAnd = query(hospitalRef, where(depArray[0], "==", true));
+	for ( var i = 1; i < depArray.length; i++ ){
+		queryAnd = query(queryAnd, where(depArray[i], "==", true))
+	};
+	const hospitals = [] ;
+	const querySnapshot = await getDocs(queryAnd);
+	querySnapshot.forEach((doc) => { // 病院の情報を取得 & consoleに表示
+			//console.log(doc.id, " => ", doc.data());
+			//console.log(doc.data().name)//フィールド取得
+			hospitals.push(doc.data());//リストで返す
+			//console.log(hospitals[0].place.latitude);
+	});
+	//console.log(hospitals);
+	return hospitals;
+}
+
+/*
+関数概要：引数の診療科がひとつでも当てはまる病院のリストを取得
+引数：診療科のリスト
+戻り値：引数のリストがひとつでも満たす病院のリスト
+*/
+async function getHospitalsOr(depArray){
+	const hospitalRef = collection(db, "hospital");
+	let hospitalName = {} //空の辞書作成
+	for ( var i = 0; i < depArray.length; i++ ){
+		var q = query(hospitalRef, where(depArray[i], "==", true));
+		const querySnapshot = await getDocs(q);
+		querySnapshot.forEach((doc) => {
+		hospitalName[doc.data().name] = doc.data() // keyが重複した場合、後に追加したデータが優先される
+		});
+	};
+	//console.log(hospitalName["高須クリニック0"])
+	const hospitals = Object.values(hospitalName) // 辞書からvalue(病院data)の一覧を取得
+	return hospitals ;
+}
 /*
 関数概要：depertmentドキュメントの日本語か英語の科の名前を取得
 引数：ja 文字列
@@ -104,9 +145,40 @@ async function getDepartment(language){
 	});
 	return depList;
 };
-/*関数*/
 
-/*IDに対するアクション*/
+/*
+関数概要：受け入れ人数を病院単位で変更
+引数1：病院名
+引数2：人数
+戻り値：なし
+*/
+async function updatenumOfAccepted(hospitalName,num){
+	const hospitalRef = collection(db, "hospital");
+	const searchHospital = document.getElementById(hospitalName).value;
+	const q = query(hospitalRef, where("name", "==", searchHospital ));
+	const querySnapshot = await getDocs(q);
+	//console.log(querySnapshot);
+
+	//当てはまる病院の受け入れ人数を更新
+	querySnapshot.forEach(async (doc) => { 
+		//ドキュメントIDと受け入れ人数を出力
+		//console.log(doc.id, " => ", doc.data());
+		//console.log(doc.data().numOfAccepted);
+		//テキストボックスに入力された文字列を10進数に変換
+		const numOfAcceptedIDValue = document.getElementById(num).value;
+		const numOfAcceptedIDNumber = parseInt(numOfAcceptedIDValue, 10);
+
+		//ドキュメントIDに対して受け入れ人数を更新
+		const hospitalDocRef = doc.ref;
+		await updateDoc(hospitalDocRef,{
+		numOfAccepted: numOfAcceptedIDNumber
+		});
+		console.log("update numOfAccepted")
+		});
+}
+/*ここまで関数*/
+
+/*ここからIDに対するアクション*/
 //内科ボタンをクリックされると以下の関数が実行される。　リクエストを作成
 $("#dep00").on("click",async function() {
 	const a =  await addDoc(requestRef,{
@@ -120,42 +192,21 @@ $("#dep00").on("click",async function() {
 
 //hospitalArrayの情報とすべて当てはまる病院を表示(AND検索)
 const hospitalArray = ['department.0', 'department.1','department.2']//仮に与えられたとする
+const now = new google.maps.LatLng(35.5988, 139.6506); //仮に救急車の現在地を都市大の位置とする
 $("#searchAnd").on("click",async function(){
-	const hospitalRef = collection(db, "hospital");
-	let queryAnd = query(hospitalRef, where(hospitalArray[0], "==", true));
-	for ( var i = 1; i < hospitalArray.length; i++ ){
-			queryAnd = query(queryAnd, where(hospitalArray[i], "==", true))
-	};
-
-	const hospitals = []
-	const querySnapshot = await getDocs(queryAnd);
-	querySnapshot.forEach((doc) => { // 病院の情報を取得 & consoleに表示
-			console.log(doc.id, " => ", doc.data());
-			console.log(doc.data().name)//フィールド取得
-			hospitals.push(doc.data());//リストで返す
-			console.log(hospitals[0].place.latitude);
-	});
-	getSortHospitalList(hospitals);//緯度経度から距離を算出する関数を実行
-	});
+	const hospitals = await getHospitalsAnd(hospitalArray);//選択されたすべての診療科が対応している病院を取得
+	getSortHospitalList(hospitals,now);//現在地から近い順にソート
+	console.log(hospitals);
+});
 
 
 	//hospitalArrayの情報とひとつでも当てはまる病院を表示(OR検索)
-	$("#searchOr").on("click",async function(){
-	const hospitalRef = collection(db, "hospital");
-	let hospitalName = {} //空の辞書作成
-	for ( var i = 0; i < hospitalArray.length; i++ ){
-			var q = query(hospitalRef, where(hospitalArray[i], "==", true));
-			const querySnapshot = await getDocs(q);
-			querySnapshot.forEach((doc) => {
-			hospitalName[doc.data().name] = doc.data() // keyが重複した場合、後に追加したデータが優先される
-			});
-	};
-	console.log(hospitalName["高須クリニック0"])
-	const hospitals = Object.values(hospitalName) // 辞書からvalue(病院data)の一覧を取得
-	getSortHospitalList(hospitals);
+$("#searchOr").on("click",async function(){
+	const hospitals = await getHospitalsOr(hospitalArray);
+	getSortHospitalList(hospitals,now);
+	console.log(hospitals);
 });
 
-//病院のデータベース
 
 //病院を新規に登録
 $("#submit").on("click",async function() {
@@ -168,35 +219,13 @@ $("#submit").on("click",async function() {
 	});
 });
 
-
-
 //病院の受け入れ人数変更
 $("#update").on("click",async function(){
-//病院名検索
-	const hospitalRef = collection(db, "hospital");
-	const searchHospital = document.getElementById("hospitalName").value;
-	const q = query(hospitalRef, where("name", "==", searchHospital ));
-	const querySnapshot = await getDocs(q);
-	console.log(querySnapshot);
-
-	//当てはまる病院の受け入れ人数を更新
-	querySnapshot.forEach(async (doc) => { 
-		//ドキュメントIDと受け入れ人数を出力
-		console.log(doc.id, " => ", doc.data());
-		console.log(doc.data().numOfAccepted);
-		
-		//テキストボックスに入力された文字列を10進数に変換
-		const numOfAcceptedIDValue = document.getElementById("numOfAcceptedID").value;
-		const numOfAcceptedIDNumber = parseInt(numOfAcceptedIDValue, 10);
-
-		//ドキュメントIDに対して受け入れ人数を更新
-		const hospitalDocRef = doc.ref;
-		await updateDoc(hospitalDocRef,{
-		numOfAccepted: numOfAcceptedIDNumber
-		});
-		console.log("update numOfAccepted")
-		});
+	//病院名検索
+	updatenumOfAccepted("hospitalName","numOfAcceptedID");
 });
-/*IDに対するアクション*/
-const tmp =getDepartment("ja");
+/*ここまでIDに対するアクション*/
+
+/*診療科を取得*/
+const tmp = await getDepartment("ja");
 console.log(tmp);
