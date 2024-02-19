1f6ae0f4d2930c155e5722b6ac505067b604eee2
Yuki <32705755+Yu-HaruWolf@users.noreply.github.com>
2024-02-20 01:13:28 +0900

    全てのリクエスト一覧を表示させる機能の追加

--- frontend/lib/request_list_page.dart
+++ frontend/lib/request_list_page.dart
@@ -1,11 +1,29 @@
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:frontend/request.dart';
 import 'package:provider/provider.dart';
 
 import 'app_state.dart';
 import 'custom_widgets/text_with_icon.dart';
 
-class RequestListPage extends StatelessWidget {
+class RequestListPage extends StatefulWidget {
   const RequestListPage({super.key});
+
+  @override
+  State<RequestListPage> createState() => _RequestListPageState();
+}
+
+class _RequestListPageState extends State<RequestListPage> {
+  List<Request> requests = [];
+  late StreamSubscription<QuerySnapshot> documentSubscription;
+
+  @override
+  void initState() {
+    super.initState();
+    getRequestList();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,21 +35,45 @@ class RequestListPage extends StatelessWidget {
       child: SingleChildScrollView(
         child: Column(
           children: [
-            for (int i = 0; i < 5; i++)
-              const HospitalCard(
-                  title: 'Title',
-                  department: 'Department',
-                  lastUpdateTime: '2000/1/1 12:34:56',
-                  id: 'aaa'),
+            for (Request request in requests)
+              RequestCard(
+                  title: request.patient,
+                  department: request.hospital,
+                  lastUpdateTime: request.lastChatTime.toDate().toString(),
+                  id: request.id),
           ],
         ),
       ),
     );
   }
+
+  Future<void> getRequestList() async {
+    documentSubscription = FirebaseFirestore.instance
+        .collection('request')
+        .orderBy('timeOfLastChat', descending: true)
+        .snapshots()
+        .listen((event) {
+      setState(() {
+        requests = [];
+        for (var doc in event.docs) {
+          requests.add(Request(
+              id: doc.id,
+              status: doc.data()['status'],
+              hospital: doc.data()['hospital'],
+              ambulance: doc.data()['ambulance'],
+              patient: doc.data()['patient'],
+              createTime: doc.data()['timeOfCreatingRequest'],
+              lastChatTime: doc.data()['timeOfLastChat'],
+              responseTime: doc.data()['timeOfResponse']));
+        }
+      });
+    });
+    return;
+  }
 }
 
-class HospitalCard extends StatelessWidget {
-  const HospitalCard({
+class RequestCard extends StatelessWidget {
+  const RequestCard({
     super.key,
     required this.title,
     required this.department,
@@ -39,14 +81,31 @@ class HospitalCard extends StatelessWidget {
     required this.id,
   });
 
-  final String title;
+  final List<dynamic> title;
   final String department;
   final String lastUpdateTime;
   final String id;
 
+  Future<String> getDepartmentsName(List<dynamic> docs) async {
+    String result = "";
+    for (DocumentReference<Map<String, dynamic>> doc in docs) {
+      DocumentSnapshot<Map<String, dynamic>> snapshot = await doc.get();
+      String name = snapshot.data()!['ja'];
+      if (result != "") {
+        result += ",$name";
+      } else {
+        result += name;
+      }
+    }
+    if (result == "") result = "No Department";
+    return result;
+  }
+
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<ApplicationState>();
+    var hospital =
+        FirebaseFirestore.instance.collection('hospital').doc(department).get();
     TextStyle nameStyle = const TextStyle(
       fontSize: 20,
     );
@@ -65,14 +124,22 @@ class HospitalCard extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                TextWithIcon(
-                    iconData: Icons.fact_check,
-                    text: title,
-                    textStyle: nameStyle),
-                TextWithIcon(
-                    textStyle: normalStyle,
-                    iconData: Icons.domain,
-                    text: department),
+                FutureBuilder(
+                    future: getDepartmentsName(title),
+                    builder: (context, snapshot) {
+                      return TextWithIcon(
+                          iconData: Icons.fact_check,
+                          text: snapshot.hasData ? snapshot.data! : '',
+                          textStyle: nameStyle);
+                    }),
+                FutureBuilder(
+                    future: hospital,
+                    builder: (context, snapshot) {
+                      return TextWithIcon(
+                          textStyle: normalStyle,
+                          iconData: Icons.domain,
+                          text: snapshot.hasData ? snapshot.data!['name'] : '');
+                    }),
                 TextWithIcon(
                     textStyle: normalStyle,
                     iconData: Icons.schedule,
